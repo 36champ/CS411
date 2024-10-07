0e9f4b417afd83787d5b1a2b7b509a35390dd02c
Mutiraj Laksanawisit <mutiraj@bu.edu>
2024-10-06 22:15:47 -0400

    Partition

---
+++ HW3 Design/wildlife_tracker/migration_tracking/migration_manager.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+
+class MigrationManager:
+
+    def cancel_migration(migration_id: int) -> None:
+        pass
+
+    def get_migration_by_id(migration_id: int) -> Migration:
+        pass
+
+    def get_migration_details(migration_id: int) -> dict[str, Any]:
+        pass
+
+    def get_migrations() -> list[Migration]:
+        pass
+
+    def get_migrations_by_current_location(current_location: str) -> list[Migration]:
+        pass
+
+    def get_migrations_by_migration_path(migration_path_id: int) -> list[Migration]:
+        pass
+
+    def get_migrations_by_start_date(start_date: str) -> list[Migration]:
+        pass
+
+    def get_migrations_by_status(status: str) -> list[Migration]:
+        pass
+
+    def schedule_migration(migration_path: MigrationPath) -> None:
+        pass
+
+    def update_migration_details(migration_id: int, **kwargs: Any) -> None:
+        pass
---
+++ HW3 Design/wildlife_tracker/migration_tracking/migration_path.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+class MigrationPath:
+
+    def create_migration_path(species: str, start_location: Habitat, destination: Habitat, duration: Optional[int] = None) -> None:
+        pass
+
+    def get_migration_path_by_id(path_id: int) -> MigrationPath:
+        pass
+
+    def get_migration_paths() -> list[MigrationPath]:
+        pass
+
+    def get_migration_paths_by_destination(destination: Habitat) -> list[MigrationPath]:
+        pass
+
+    def get_migration_paths_by_species(species: str) -> list[MigrationPath]:
+        pass
+
+    def get_migration_paths_by_start_location(start_location: Habitat) -> list[MigrationPath]:
+        pass
+
+    def get_migration_path_details(path_id) -> dict:
+        pass
+
+    def remove_migration_path(path_id: int) -> None:
+        pass
+
+    def update_migration_path_details(path_id: int, **kwargs) -> None:
+        pass
